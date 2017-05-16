c2cf534547e99f1121b1d3f91b07f48f794e1034
39Er <liuweiminfirst@163.com>
2017-05-16 10:54:27 +0800

    add ChildProcess util and ftp util

--- commonUtil.js
+++ commonUtil.js
@@ -1,5 +1,8 @@
 'use strict';
 
 const config = require('config');
+const bunyan = require('bunyan');
 
 module.exports.config = config;
+module.exports.logger = bunyan.createLogger(config.get('loggerConfig'));
+
